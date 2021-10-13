18ef1ee89c690a95f38063889d3ee9f87377296a
Hans Baier <hansfbaier@gmail.com>
2021-10-13 14:22:08 +0700

    mandelbrot-app: factor out a class for the view

--- mandelbrot-app.py
+++ mandelbrot-app.py
@@ -17,15 +17,19 @@ if debug: print(dev)
 
 scale = 8*8
 
+def fix2float(fix):
+    return fix/2**scale
 
-def send_command(bytewidth, no_pixels_x, no_pixels_y, max_iterations, bottom_left_corner_x, bottom_left_corner_y, step, debug=False):
-    command_bytes = struct.pack("HHI", no_pixels_x, no_pixels_y, max_iterations)
+def float2fix(f):
+    return int(f*2**scale)
 
-    command_bytes += bottom_left_corner_x.to_bytes(bytewidth, byteorder='little', signed=True)
-    command_bytes += bottom_left_corner_y.to_bytes(bytewidth, byteorder='little', signed=True)
-    command_bytes += step.to_bytes(bytewidth, byteorder='little', signed=True)
+def send_command(bytewidth, view, debug=False):
+    command_bytes = struct.pack("HHI", view.width-1, view.height-1, view.max_iterations)
+    command_bytes += view.corner_x.to_bytes(bytewidth, byteorder='little', signed=True)
+    command_bytes += view.corner_y.to_bytes(bytewidth, byteorder='little', signed=True)
+    command_bytes += view.step    .to_bytes(bytewidth, byteorder='little', signed=True)
     command_bytes += bytes([0xa5])
-    if debug: print(f"command: {str(bytes(command_bytes))}")
+    if debug: print(f"command: {[hex(b) for b in command_bytes]}")
 
     dev.write(0x01, command_bytes)
 
@@ -61,27 +65,35 @@ def openImage(path):
                                   'darwin':'open'}[sys.platform]
     subprocess.run([imageViewerFromCommandLine, path])
 
-corner_x = -2 << scale
-corner_y = -5 << (scale - 2)
-step = 1 << (scale - 9)
+class FractalView():
+    def __init__(self, *, center_x, center_y, radius, width=1920, height=1080, max_iterations=256) -> None:
+        radius_pixels = min(width, height) / 2
+        step = radius / radius_pixels / 2
 
-image_width  =  1920 # 256+128
-image_height =  1300 # 256+64
+        self.step = float2fix(step)
+        self.corner_x = corner_x = float2fix(center_x - (width/2)  * step)
+        self.corner_y = corner_y = float2fix(center_y - (height/2) * step)
+        self.width  = width
+        self.height = height
+        self.max_iterations = max_iterations
+        if debug:
+            print(f"left_corner_x: {corner_x/2**scale} corner_y: {corner_y/2**scale}, step: {self.step/2**scale}")
+            print(f"right_corner_x: {(corner_x + width*self.step)/2**scale} upper_corner_y: {(corner_y + height*self.step)/2**scale}")
 
-def fix2float(fix):
-    return fix/2**scale
 
-print(f"left_corner_x: {corner_x/2**scale} corner_y: {corner_y/2**scale}, step: {step/2**scale}")
-print(f"right_corner_x: {(corner_x + image_width*step)/2**scale} upper_corner_y: {(corner_y + image_height*step)/2**scale}")
+default_view = FractalView(center_x=-0.75,    center_y=0,             radius=2.5,         max_iterations=170)
+swirl        = FractalView(center_x=-0.74791, center_y=0.0888909763, radius=6.9921e-5,   max_iterations=4096)
+
+view = default_view
 
 from sys import argv
 if __name__ == "__main__":
     if len(argv) > 1:
         if argv[1] == "plot":
-            p = send_command(9, image_width, image_height, 256, corner_x, corner_y, step, debug=False)
+            p = send_command(9, view, debug=False)
             r = [e for e in p if e[3] & 0x1 > 0]
-            x = [fix2float(corner_x) + fix2float(step) * e[0] for e in r]
-            y = [fix2float(corner_y) + fix2float(step) * e[1] for e in r]
+            x = [fix2float(view.corner_x) + fix2float(view.step) * e[0] for e in r]
+            y = [fix2float(view.corner_y) + fix2float(view.step) * e[1] for e in r]
 
             coords = list(zip(x, y))
             print(coords)
@@ -91,21 +103,20 @@ if __name__ == "__main__":
 
             # To show the plot
             plt.show()
-            code.interact(local=locals())
 
         if argv[1] == "debug":
-            send_command(9, image_width, image_height, 255, corner_x, corner_y, step, debug=True)
+            send_command(9, view, debug=True)
             code.interact(local=locals())
     else:
         tstart = time.perf_counter()
-        pixels = send_command(9, image_width - 1, image_height - 1, 255, corner_x, corner_y, step)
+        pixels = send_command(9, view, debug=False)
         tusb = time.perf_counter()
         print(f"USB transfer took: {tusb - tstart:0.4f} seconds")
 
         import numpy as np
         from matplotlib.image import imsave
 
-        p = np.zeros((image_height, image_width, 3))
+        p = np.zeros((view.height, view.width, 3))
 
         for pixel in pixels:
             x     = pixel[0]
@@ -114,10 +125,10 @@ if __name__ == "__main__":
             green = pixel[3] / 255.0
             blue  = pixel[4] / 255.0
 
-            if x >= image_width:
+            if x >= view.width:
                 print(f"rogue pixel: {str(pixel)}")
                 continue
-            if y >= image_height:
+            if y >= view.height:
                 print(f"rogue pixel: {str(pixel)}")
                 continue
 
