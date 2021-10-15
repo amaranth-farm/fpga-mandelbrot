0597fae1ddb925758157bffc61154c10fef561bb
Hans Baier <hansfbaier@gmail.com>
2021-10-15 11:48:41 +0700

    mandelbrot-app: Add Gtk GUI

--- software/mandelbrot-app.py
+++ software/mandelbrot-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # this is a throwaway prototype
-# so the code is highly experimental
+# so the code is highly experimental and sketchy
 
 import time
 import struct
@@ -64,23 +64,35 @@ def openImage(path):
     subprocess.run([imageViewerFromCommandLine, path])
 
 class FractalView():
-    def __init__(self, *, center_x, center_y, radius, width=1920, height=1080, max_iterations=256) -> None:
+    def __init__(self, *, center_x, center_y, radius, width, height, max_iterations=256) -> None:
+        self.update(center_x=center_x, center_y=center_y, radius=radius, width=width, height=height, max_iterations=max_iterations)
+
+    def update(self, *, center_x, center_y, radius, width, height, max_iterations=256) -> None:
         radius_pixels = min(width, height) / 2
-        step = radius / radius_pixels / 2
+        step = radius / radius_pixels
 
         self.step = float2fix(step)
-        self.corner_x = corner_x = float2fix(center_x - (width/2)  * step)
-        self.corner_y = corner_y = float2fix(center_y - (height/2) * step)
+        self.corner_x = float2fix(center_x - (width/2)  * step)
+        self.corner_y = float2fix(center_y - (height/2) * step)
         self.width  = width
         self.height = height
         self.max_iterations = max_iterations
-        if debug:
-            print(f"left_corner_x: {corner_x/2**scale} corner_y: {corner_y/2**scale}, step: {self.step/2**scale}")
-            print(f"right_corner_x: {(corner_x + width*self.step)/2**scale} upper_corner_y: {(corner_y + height*self.step)/2**scale}")
 
+    def get_center(self):
+        center_x = fix2float(self.corner_x) + fix2float(self.step) * (self.width  / 2)
+        center_y = fix2float(self.corner_y) + fix2float(self.step) * (self.height / 2)
+        return [center_x, center_y]
 
-default_view = FractalView(center_x=-0.75,    center_y=0,             radius=2.5,         max_iterations=170, width=1550, height=1080)
-swirl        = FractalView(center_x=-0.74791, center_y=0.0888909763,  radius=6.9921e-5,   max_iterations=4096)
+    def get_radius(self):
+        radius_pixels = min(self.width, self.height) / 2
+        return fix2float(self.step) * radius_pixels
+
+    def to_string(self):
+        x, y = self.get_center()
+        return f"center_x: {x}, center_y: {y}, radius: {self.get_radius()}"
+
+default_view = FractalView(center_x=-0.75,    center_y=0,             radius=2.5,         max_iterations=170,  width=1550, height=1080)
+swirl        = FractalView(center_x=-0.74791, center_y=0.0888909763,  radius=6.9921e-5,   max_iterations=4096, width=1550, height=1080)
 
 view = default_view
 
@@ -110,7 +122,135 @@ if __name__ == "__main__":
     if len(argv) > 1:
         if argv[1] == "debug":
             send_command(9, view, debug=True)
-            code.interact(local=locals())
+
+        if argv[1] == "gui":
+            import gi
+            gi.require_version("Gtk", "3.0")
+            from gi.repository     import GLib, Gtk, Gdk
+            from gi.repository.Gtk import DrawingArea
+            import cairo
+
+            class GuiHandler:
+                surface = None
+                builder = None
+                canvas  = None
+
+                width  = 0
+                weight = 0
+
+                drawing = False
+                view = default_view
+
+                def __init__(self, builder) -> None:
+                    self.builder = builder
+                    centerx_widget    = builder.get_object("center_x")
+                    centery_widget    = builder.get_object("center_y")
+                    radius_widget     = builder.get_object("radius")
+                    iterations_widget = builder.get_object("iterations")
+
+                    center_x, center_y = view.get_center()
+                    radius             = view.get_radius()
+                    iterations         = view.max_iterations
+
+                    centerx_widget   .set_text(str(center_x))
+                    centery_widget   .set_text(str(center_y))
+                    radius_widget    .set_text(str(radius))
+                    iterations_widget.set_text(str(iterations))
+
+                    self.updateImageSurfaceIfNeeded()
+
+                def painter(self):
+                    drawing_start = time.perf_counter()
+                    try:
+                        while True:
+                            # get() will exit this thread if the
+                            # queue is empty
+                            pixel = pixel_queue.get()
+
+                            x     = pixel[0]
+                            y     = pixel[1]
+
+                            red, green, blue = colortable[pixel[2] & 0xf]
+                            red   /= 255.0
+                            green /= 255.0
+                            blue  /= 255.0
+
+                            maxed = pixel[2] >> 7
+
+                            def draw_pixel():
+                                cr = cairo.Context(self.surface)
+                                if not maxed:
+                                    cr.set_source_rgb(red, green, blue)
+                                else:
+                                    cr.set_source_rgb(0, 0, 0)
+
+                                cr.rectangle(x, y, 1.5, 1.5)
+                                cr.fill()
+                                self.surface.mark_dirty_rectangle(x, y, 1, 1)
+                                self.canvas.queue_draw()
+                                return False
+
+                            Gdk.threads_enter()
+                            draw_pixel()
+                            Gdk.threads_leave()
+                            pixel_queue.task_done()
+
+                    finally:
+                        now = time.perf_counter()
+                        print(f"drawing took: {now - drawing_start:0.4f} seconds")
+
+                def onDestroy(self, *args):
+                    Gtk.main_quit()
+
+                def updateImageSurfaceIfNeeded(self):
+                    self.canvas = canvas = builder.get_object("canvas")
+                    width  = canvas.get_allocated_size().allocation.width
+                    height = canvas.get_allocated_size().allocation.height
+                    if self.surface is None or self.width != width or self.height != height:
+                        self.surface = cairo.ImageSurface(cairo.FORMAT_RGB24, width, height)
+                        self.width, self.height = width, height
+
+                def onButtonPressed(self, button):
+                    self.updateImageSurfaceIfNeeded()
+
+                    center_x   = float(builder.get_object("center_x").get_text())
+                    center_y   = float(builder.get_object("center_y").get_text())
+                    radius     = float(builder.get_object("radius").get_text())
+                    iterations = int  (builder.get_object("iterations").get_text())
+
+                    self.view.update(center_x=center_x, center_y=center_y, radius=radius, width=self.width, height=self.height, max_iterations=iterations)
+                    print(self.view.to_string())
+
+                    cr = cairo.Context(self.surface)
+                    cr.set_source_rgb(0, 0, 0)
+                    cr.rectangle(0, 0, self.width, self.height)
+                    cr.fill()
+
+                    view = self.view
+                    view.width  = self.width
+                    view.height = self.height
+                    usb_reader = lambda: send_command(9, view, debug=False)
+                    usb_thread = threading.Thread(target=usb_reader, daemon=True)
+                    usb_thread.start()
+
+                    painter_thread = threading.Thread(target=lambda: self.painter(), daemon=True)
+                    painter_thread.start()
+
+                def onDraw(self, canvas: DrawingArea, cr: cairo.Context):
+                    cr.set_source_surface(self.surface, 0, 0)
+                    cr.paint()
+
+            builder = Gtk.Builder()
+            builder.add_from_file("mandelbrot-client-gui.glade")
+            handler = GuiHandler(builder)
+            builder.connect_signals(handler)
+
+            window = builder.get_object("window")
+            window.connect("destroy", Gtk.main_quit)
+            window.show_all()
+
+            Gdk.threads_init()
+            Gtk.main()
     else:
         tstart = time.perf_counter()
 
@@ -138,7 +278,7 @@ if __name__ == "__main__":
 
                 red, green, blue = colortable[pixel[2] & 0xf]
                 maxed = pixel[2] >> 7
-                if not maxed > 0:
+                if not maxed:
                     p[y][x][0] = red   / 255.0
                     p[y][x][1] = green / 255.0
                     p[y][x][2] = blue  / 255.0
